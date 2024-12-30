823660ed4b9c99c28004cabee55ac4fa4213ff35
Dorokhovich Michael <mikhail@dorokhovich.ru>
2024-12-30 13:39:43 +0100

    polishing

--- keycloak/docker-entrypoint.sh
+++ keycloak/docker-entrypoint.sh
@@ -1,27 +1,6 @@
 #!/bin/sh
 set -e
 
-echo "----------$(date '+%Y-%m-%d %H:%M:%S')----------"
-echo ls -al /opt/keycloak
-ls -al /opt/keycloak
-echo "========================================"
-echo "----------$(date '+%Y-%m-%d %H:%M:%S')----------"
-echo ls -al /opt/keycloak/conf
-ls -al /opt/keycloak/conf
-echo "========================================"
-echo "----------$(date '+%Y-%m-%d %H:%M:%S')----------"
-echo cat /opt/keycloak/conf/keycloak.conf
-cat /opt/keycloak/conf/keycloak.conf
-echo "========================================"
-echo "----------$(date '+%Y-%m-%d %H:%M:%S')----------"
-echo ls -al /opt/keycloak/conf/cache-ispn-jdbc-ping.xml
-ls -al /opt/keycloak/conf/cache-ispn-jdbc-ping.xml
-echo "========================================"
-echo "----------$(date '+%Y-%m-%d %H:%M:%S')----------"
-echo cat /opt/keycloak/conf/cache-ispn-jdbc-ping.xml
-cat /opt/keycloak/conf/cache-ispn-jdbc-ping.xml
-echo "========================================"
-
 exec /opt/keycloak/bin/kc.sh start \
   --optimized \
   --verbose \
